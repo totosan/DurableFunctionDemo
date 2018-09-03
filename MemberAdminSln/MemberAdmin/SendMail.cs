32cadc5c6f881760d3e5db754e0910278420577f
Thomas Tomow <toto_san@live.com>
2018-09-03 14:58:07 +0200

    initial version

---
+++ MemberAdminSln/MemberAdmin/SendMail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemberAdmin
+{
+	public class SendMail
+	{
+		public string emailadress { get; set; }
+		public string emailSubject { get; set; }
+		public string emailBody { get; set; }
+	}
+}
